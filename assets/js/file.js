a2c12d1ffe52917b88f58a9a843e87d6677e43f1
Ravindra <rscipher001@gmail.com>
2019-05-12 21:07:05 +0530

    hacky sack

--- assets/js/file.js
+++
@@ -1,1 +0,0 @@
-document.querySelector("#saveButton").addEventListener("click",function(){var e=document.querySelector("#title"),t=document.querySelector("#data"),c=e.value,l=t.value;console.log(c+" : "+l),db.collection("notes").add({title:c,data:l}).then(function(e){alert("Saved successfully with id: "+e.id)}).catch(function(e){alert("Failed "+e)})});
